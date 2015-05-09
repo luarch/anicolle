2fcd9a60e36f2bdf7065897a94c953eff430e8da
Archie Lu <chienius@outlook.com>
2015-05-09 16:24:24 +0800

    Rebuilt with module

---
+++ anicolle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""AniColle Library
+
+Collect your animes like a geek.
+"""
+
+import sqlite3 as _sqlite3
+import re as _re
+import urllib.request as _ur
+import urllib.parse as _up
+
+def dbInit( dbname="bgmarker.db" ):
+    global sqlcon, sqlcur
+    sqlcon = _sqlite3.connect( dbname )
+    sqlcur = sqlcon.cursor()
+    sqlcur.execute("CREATE TABLE IF NOT EXISTS bangumi ( id INTEGER PRIMARY KEY AUTOINCREMENT, name, cur_epi DEFAULT 0, on_air_epi DEFAULT 0, on_air_day DEFAULT 0 , `chk_key` DEFAULT '');")
+
+# def getName( bid ):
+#     name = sqlcur.execute("SELECT `name` FROM `bangumi` WHERE `id` = ?", (str(bid),) ).fetchone()
+#     return name[0]
+
+def getAni( bid=-1, on_air_day=-1 ):
+    sqlcmd = "SELECT `id`, `name`, `cur_epi`, `on_air_day` from `bangumi`";
+    if bid>=0:
+        sqlcmd += " WHERE `id` = " + str(bid)
+    if on_air_day>=0:
+        sqlcmd += " WHERE `on_air_day` = " + str(on_air_day)
+    sqlcmd += " ORDER BY `on_air_day` ASC"
+    r = []
+    for row in sqlcur.execute( sqlcmd ).fetchall():
+        r.append(row)
+    return r
+
+def add(name, on_air_day=0, cur_epi=0, chk_key=""):
+    return sqlcur.execute( "INSERT INTO `bangumi`( `name`, `on_air_day`, `cur_epi`, `chk_key` ) VALUES( ?, ?, ?, ? )", ( name, on_air_day, cur_epi, chk_key ) )
+
+def remove(bid):
+    return sqlcur.execute( "DELETE FROM `bangumi` WHERE `id` = ?", (str(bid), ) )
+
+def plus( bid ):
+    return sqlcur.execute("UPDATE `bangumi` SET `cur_epi`=`cur_epi`+1 WHERE `id` = ?", (str(bid),) )
+
+def decrease( bid ):
+    return sqlcur.execute("UPDATE `bangumi` SET `cur_epi`=`cur_epi`-1 WHERE `id` = ?", (str(bid),) )
+
+def chkup( bid ):
+    name = sqlcur.execute("SELECT `name`, `chk_key`, `cur_epi`, `on_air_day` FROM `bangumi` WHERE `id` = ?", (str(bid),) ).fetchone()
+    if not name:
+        raise NameError
+    if not name[1] or not name[3]:
+        return 0
+    tepi = int(name[2])+1
+    chkkey = str(name[1]) + " " + "[%02d] MP4"%tepi
+    name = name[0]
+    chkkey = _up.quote_plus(chkkey)
+    r = _ur.urlopen( "http://share.popgo.org/search.php?title=%s&sorts=1"%chkkey ).read().decode('utf-8')
+    re1 = _re.compile( '查看详情页.*?title="([^"]*' + '%02d'%tepi + '[集话\]】\[][^"]*)".*?(magnet[^"]*)"' )
+    maglink = _re.search(re1, r)
+    if maglink:
+        magname = maglink.group(1)
+        maglink = maglink.group(2)
+        return { "magname": magname, "maglink": maglink }
+    else:
+        raise LookupError
+
+
--- main.py
+++ main.py
@@ -1,116 +1,79 @@
 #!/usr/bin/env python
-import sqlite3
-import re
-import urllib.request
-import urllib.parse
 import argparse
+import anicolle
 import os
 
-def chkup(bid, showPrompt=0):
-    name = c.execute("SELECT `name`, `chk_key`, `cur_epi` FROM `bangumi` WHERE `id` = ?", (str(bid),) ).fetchone()
-    if not name:
-        raise NameError
-    tepi = int(name[2])+1
-    chkkey = str(name[1]) + " " + "[%02d] MP4"%tepi
-    name = name[0]
-    if showPrompt:
-        print("查找 %s 的第 %s 集资源"%( name, str(tepi) ) )
-    chkkey = urllib.parse.quote_plus(chkkey)
-    r = urllib.request.urlopen( "http://share.popgo.org/search.php?title=%s&sorts=1"%chkkey ).read().decode('utf-8')
-    # re1 = re.compile('(magnet[^"]*)"')
-    # re2 = re.compile('查看详情页[^=]*="_blank" title="([^"]*' + '%02d'%str(tepi) + '(集|话|\]|】)[^"]*)"')
-    re1 = re.compile( '查看详情页.*?title="([^"]*' + '%02d'%tepi + '[集话\]】\[][^"]*)".*?(magnet[^"]*)"' )
-    maglink = re.search(re1, r)
-    if maglink:
-        magname = maglink.group(1)
-        maglink = maglink.group(2)
-        return { "magname": magname, "maglink": maglink }
-        # print("%s\n%s"%(magname, maglink))
-        # if os.system('echo "%s" | xclip -in -selection clipboard'%maglink) == 0:
-        #     print("磁力链接已拷贝到剪贴板")
-    else:
-        raise LookupError
-
+anicolle.dbInit()
 
 aparser = argparse.ArgumentParser()
-aparser.add_argument( "-a", "--add", nargs=4, metavar=("NAME","ONAIR","CUREPI", "CHKKEY"), help="Add a bgm." )
-aparser.add_argument( "-rm", "--remove", type=int, metavar="ID", help="Delete a bgm." )
-aparser.add_argument( "-p", "--plus", type=int, metavar="ID", help="Plus 1 to the bgm specified." )
-aparser.add_argument( "-d", "--decrease", type=int, metavar="ID", help="Decrease 1 to the bgm specified." )
-aparser.add_argument( "-s", "--show", type=int, metavar="ID", help="Display specified bangumi.", default=-1 )
-aparser.add_argument( "-t", "--showbyday", type=int, metavar="WEEKDAY", choices=range(0,8), help="Display bangumi on specified onair day.", default=-1 )
-aparser.add_argument( "-c", "--chkup", type=int, metavar="ID", help="Checkup latest updates of the specified bangumi and returns the magnet link to download", const=-1, nargs='?' )
+aparser.add_argument(
+    "-a", "--add", nargs=4,
+    metavar=("NAME","ONAIR","CUREPI", "CHKKEY"), help="Add a bgm."
+)
+aparser.add_argument(
+    "-rm", "--remove", type=int,
+    metavar="ID", help="Delete a bgm."
+)
+aparser.add_argument(
+    "-p", "--plus", type=int,
+    metavar="ID", help="Plus 1 to the bgm specified."
+)
+aparser.add_argument(
+    "-d", "--decrease", type=int,
+    metavar="ID", help="Decrease 1 to the bgm specified."
+)
+aparser.add_argument(
+    "-s", "--show", type=int,
+    metavar="ID", help="Display specified bangumi.", default=-1
+)
+aparser.add_argument(
+    "-t", "--showbyday", type=int,
+    metavar="WEEKDAY", choices=range(0,8),
+    help="Display bangumi on specified onair day.", default=-1
+)
+aparser.add_argument(
+    "-c", "--chkup", type=int,
+    metavar="ID",
+    help="Checkup latest updates of the specified bangumi and returns the magnet link to download",
+    const=-1, nargs='?'
+)
 args = aparser.parse_args()
 
-sqlcon = sqlite3.connect( "bgmarker.db" )
-c = sqlcon.cursor();
+
 
 if args.add:
-    c.execute( "INSERT INTO `bangumi`( `name`, `on_air_day`, `cur_epi`, `chk_key` ) VALUES( ?, ?, ?, ? )", args.add )
-    sqlcon.commit()
-    print( "%s has been added as ID %d" % ( args.add[0], c.lastrowid ) )
-elif args.remove:
-    name = c.execute("SELECT `name`, `cur_epi` FROM `bangumi` WHERE `id` = ?", (str(args.remove),) ).fetchone()
-    if name:
-        name = name[0]
-        print( "Are you sure to delete %s ? (Y/n)" % (name, ) )
-        ec = str(input())
-        if ec=='y' or ec=='Y' or ec=='':
-            c.execute( "DELETE FROM `bangumi` WHERE `id` = ?", (str(args.remove), ) )
-            sqlcon.commit()
-            print( "ID %d has been deleted. " % args.remove )
-    else:
-        print("ERR: specified bgm not found.")
+    anicolle.add( args.add[0], args.add[1], args.add[2], args.add[3] )
 elif args.plus:
-    name = c.execute("SELECT `name`, `cur_epi` FROM `bangumi` WHERE `id` = ?", (str(args.plus),) ).fetchone()
-    if name:
-        cepi = int(name[1])
-        name = name[0]
-        print( "Are you sure plus one to %s(%d) ? (Y/n)" % (name, cepi) )
-        ec = str(input())
-        if ec=='y' or ec=='Y' or ec=='':
-            r = c.execute("UPDATE `bangumi` SET `cur_epi`=`cur_epi`+1 WHERE `id` = ?", (str(args.plus),) )
-            sqlcon.commit()
-            print( "%s has been plus one to %d" % (name, cepi+1) )
-    else:
-        print("ERR: specified bgm not found.")
+    anicolle.plus( args.plus )
 elif args.decrease:
-    name = c.execute("SELECT `name`, `cur_epi` FROM `bangumi` WHERE `id` = ?", (str(args.decrease),) ).fetchone()
-    if name:
-        cepi = int(name[1])
-        name = name[0]
-        print( "Are you sure decrease one to %s(%d) ? (Y/n)" % (name, cepi) )
-        ec = str(input())
-        if ec=='y' or ec=='Y' or ec=='':
-            r = c.execute("UPDATE `bangumi` SET `cur_epi`=`cur_epi`-1 WHERE `id` = ?", (str(args.decrease),) )
-            sqlcon.commit()
-            print( "%s has been decreased one to %d" % (name, cepi-1) )
-    else:
-        print("ERR: specified bgm not found.")
+    anicolle.decrease( args.decrease )
 elif args.chkup:
     if args.chkup>0:
-        try:
-            r = chkup(args.chkup, 1)
-            print("%s\n%s"%(r['magname'], r['maglink']))
-            if os.system('echo "%s" | xclip -in -selection clipboard'%r['maglink']) == 0:
-                print("磁力链接已拷贝到剪贴板")
-        except NameError:
-            print( "ERR: specified bgm not found." )
-        except LookupError:
-            print( "没有找到相关资源" )
+        n = anicolle.getAni( args.chkup )
+        if n:
+            print( "查找 %s 的第 %d 集资源" % (n[0][1], n[0][2]+1) )
+            try:
+                r = anicolle.chkup(args.chkup)
+                print("%s\n%s"%(r['magname'], r['maglink']))
+                if os.system('echo "%s" | xclip -in -selection clipboard'%r['maglink']) == 0:
+                    print("磁力链接已拷贝到剪贴板")
+            except NameError:
+                print( "ERR: specified bgm not found." )
+            except LookupError:
+                print( "没有找到相关资源" )
     elif args.chkup==-1:
         print( "查找所有资源" )
-        sqlcmd = "SELECT `id`, `name` FROM `bangumi` WHERE `on_air_day`>0";
+        n = anicolle.getAni()
         i = 0
         mls = []
-        for row in c.execute( sqlcmd ).fetchall():
+        for row in n:
             try:
-                r = chkup(row[0])
+                r = anicolle.chkup(row[0])
             except:
                 continue
                 pass
             else:
-                print( "%-3d %s\n有更新\n" % row )
+                print( "%-3d %s\n有更新: %s\n" % (row[0], row[1], r['magname']) )
                 mls.append(r['maglink'])
                 i = i+1
         if not i:
@@ -120,12 +83,6 @@ elif args.chkup:
             if os.system('echo -e "%s" | xclip -in -selection clipboard'%( '\n'.join(mls), )) == 0:
                 print("磁力链接已拷贝到剪贴板")
 else:
-    sqlcmd = "SELECT `id`, `name`, `cur_epi`, `on_air_day` from `bangumi`";
-    if args.show>=0:
-        sqlcmd += " WHERE `id` = " + str(args.show)
-    if args.showbyday>=0:
-        sqlcmd += " WHERE `on_air_day` = " + str(args.showbyday)
-    sqlcmd += " ORDER BY `on_air_day` ASC"
-    for row in c.execute( sqlcmd ).fetchall():
-        print( "%-3d %s\n看到%s话 每周%s更新\n" % row )
-
+    r = anicolle.getAni( args.show, args.showbyday )
+    for row in r:
+        print( "%-3d %s\t已看%s/周%s更新" % row )
