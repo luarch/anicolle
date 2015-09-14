e336e2a6002d3bda9a7f4f637b919a4e598c9217
Archibald Lu <chienius@outlook.com>
2015-09-14 15:17:44 +0800

    Building skeleton for webui & add a gitignore file & add user auth function to webui

--- anicolle/webui.py
+++ anicolle/webui.py
@@ -1,63 +1,85 @@
 #!/usr/bin/env python
-import anicolle.core as core
+# import anicolle.core as core
 import json
 from socket import gethostname
-from bottle import route, run, template, request, static_file, Bottle, TEMPLATE_PATH
+from bottle import route, run, template, request, static_file, Bottle, TEMPLATE_PATH, auth_basic
 
 app = Bottle()
 workDir = "./anicolle/"
 TEMPLATE_PATH.append(workDir+"views")
 
+def user_auth( user, passwd  ):
+    if user == 'archie' and passwd == '110120':
+        return True
+    return False
+
 @app.route("/static/<path:path>")
-def home(path):
+def static(path):
     return static_file( path, root=workDir+'public' );
 
 @app.route("/")
+@app.auth_basic(user_auth)
 def home():
     return template("home", hostname = gethostname() )
 
 @app.route("/action/get/")
 @app.route("/action/get")
+@app.auth_basic(user_auth)
 def getAllBgm():
-    return json.dumps( core.getAni() )
+    # return json.dumps( core.getAni() )
+    return 0
 
 @app.route("/action/get/<bid>")
+@app.auth_basic(user_auth)
 def getBgm( bid ):
-    return json.dumps( core.getAni(int(bid)) )
+    # return json.dumps( core.getAni(int(bid)) )
+    return 0
 
 @app.route("/action/plus/<bid>")
+@app.auth_basic(user_auth)
 def plus( bid ):
-    core.plus(bid)
+    # core.plus(bid)
+    pass
 
 @app.route("/action/decrease/<bid>")
+@app.auth_basic(user_auth)
 def decrease( bid ):
-    core.decrease(bid)
+    # core.decrease(bid)
+    pass
 
 @app.post("/action/modify/<bid>")
+@app.auth_basic(user_auth)
 def modify( bid ):
     name = request.forms.name
     cur_epi = request.forms.cur_epi
     on_air = request.forms.on_air
     chk_key = request.forms.chk_key
-    core.modify( bid, name, cur_epi, on_air, chk_key )
+    # core.modify( bid, name, cur_epi, on_air, chk_key )
+    pass
 
 @app.post("/action/add")
+@app.auth_basic(user_auth)
 def add():
     name = request.forms.name
     cur_epi = request.forms.cur_epi
     on_air = request.forms.on_air
     chk_key = request.forms.chk_key
-    core.add( name, cur_epi, on_air, chk_key )
+    # core.add( name, cur_epi, on_air, chk_key )
+    pass
 
 @app.route("/action/remove/<bid>")
+@app.auth_basic(user_auth)
 def remove(bid):
-    core.remove( bid );
+    # core.remove( bid );
+    pass
 
 @app.route("/action/chkup/<bid>")
+@app.auth_basic(user_auth)
 def chkup(bid):
-    return json.dumps( core.chkup(bid) );
+    # return json.dumps( core.chkup(bid) );
+    return 0
 
-def start(port=8080):
+def startServer(port=8080):
     port = int(port)
     run(app, host='localhost', port=port)
 
