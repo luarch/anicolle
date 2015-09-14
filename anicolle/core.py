a1a4d52112ac8e0cbe8e9c7ce85d59ddbe4e5d32
Archibald Lu <chienius@outlook.com>
2015-09-14 16:39:57 +0800

    Switched the native sqlite package to an ORM named peewee.

--- anicolle/core.py
+++ anicolle/core.py
@@ -2,67 +2,97 @@
 """AniColle Library
 
 Collect your animes like a geek.
+
+Database model and operations here.
 """
 
-import sqlite3 as _sqlite3
+from peewee import *
+from .config import config
 import re as _re
 import urllib.request as _ur
 import urllib.parse as _up
 
-def dbInit( dbname="bgmarker.db" ):
-    global sqlcon, sqlcur
-    sqlcon = _sqlite3.connect( dbname )
-    sqlcur = sqlcon.cursor()
-    sqlcur.execute("CREATE TABLE IF NOT EXISTS bangumi ( id INTEGER PRIMARY KEY AUTOINCREMENT, name, cur_epi INTEGER DEFAULT 0, on_air_epi INTEGER DEFAULT 0, on_air_day INTEGER DEFAULT 0 , `chk_key` DEFAULT '');")
+db = SqliteDatabase(config['default'].DATABASE)
+
+class Bangumi(Model):
+    # `id` field is added automatically
+    name = TextField()
+    cur_epi = IntegerField(default=0)
+    on_air_epi = IntegerField(default=0)
+    on_air_day = IntegerField(default=0)
+    chk_key = TextField(default='')
+    class Meta:
+        database = db
+
+def dbInit():
+    db.connect()
+    db.create_tables([Bangumi], safe=True)
+    db.close()
 
 def getAni( bid=-1, on_air_day=-1 ):
-    sqlcmd = "SELECT `id`, `name`, `cur_epi`, `on_air_day`, `chk_key` from `bangumi`";
-    if bid>=0:
-        sqlcmd += " WHERE `id` = " + str(bid)
-    if on_air_day>=0:
-        sqlcmd += " WHERE `on_air_day` = " + str(on_air_day)
-    sqlcmd += " ORDER BY `on_air_day` ASC"
+    db.connect()
     r = []
-    for row in sqlcur.execute( sqlcmd ).fetchall():
-        r.append(list(row))
-    if bid>=0 and r:
-        r = r[0]
+    if bid>=0:
+        # get a single record
+        r = Bangumi.get(Bangumi.id==bid)
+    elif on_air_day>=0:
+        # get a set of records
+        for bgm in Bangumi.select().where(Bangumi.on_air_day==on_air_day):
+            r.append(bgm)
+    else:
+        # get all records
+        for bgm in Bangumi.select():
+            r.append(bgm)
+    db.close()
     return r
 
 def add( name, cur_epi=0, on_air_day=0, chk_key="" ):
-    sqlcur.execute(
-        "INSERT INTO `bangumi`( `name`, `on_air_day`, `cur_epi`, `chk_key` ) VALUES( ?, ?, ?, ? )",
-        ( name, int(on_air_day), int(cur_epi), chk_key )
-    )
-    sqlcon.commit()
+    db.connect()
+    bgm = Bangumi(name=name, cur_epi=cur_epi, on_air_day=on_air_day, chk_key=chk_key);
+    bgm.save()
+    db.close()
 
 def modify( bid, name, cur_epi=0, on_air_day=0, chk_key="" ):
-    sqlcur.execute( "UPDATE `bangumi` SET `name` = ? WHERE `id` = ?", ( name, bid ) )
-    sqlcur.execute( "UPDATE `bangumi` SET `cur_epi` = ? WHERE `id` = ?", ( int(cur_epi), bid ) )
-    sqlcur.execute( "UPDATE `bangumi` SET `on_air_day` = ? WHERE `id` = ?", ( int(on_air_day), bid ) )
-    sqlcur.execute( "UPDATE `bangumi` SET `chk_key` = ? WHERE `id` = ?", ( chk_key, bid ) )
-    sqlcon.commit()
+    db.connect()
+    bgm = Bangumi.get(Bangumi.id==bid)
+    bgm.name = name
+    bgm.cur_epi = int(cur_epi)
+    bgm.on_air_day = int(on_air_day)
+    bgm.chk_key = chk_key
+    bgm.save()
+    db.close()
 
 def remove(bid):
-    sqlcur.execute( "DELETE FROM `bangumi` WHERE `id` = ?", (str(bid), ) )
-    sqlcon.commit()
+    db.connect()
+    bgm = Bangumi.get(Bangumi.id==bid)
+    bgm.delete_instance()
+    db.close()
 
 def plus( bid ):
-    sqlcur.execute("UPDATE `bangumi` SET `cur_epi`=`cur_epi`+1 WHERE `id` = ?", (str(bid),) )
-    sqlcon.commit()
+    db.connect()
+    bgm = Bangumi.get(Bangumi.id==bid)
+    bgm.cur_epi = bgm.cur_epi +1
+    bgm.save()
+    db.close()
 
 def decrease( bid ):
-    sqlcur.execute("UPDATE `bangumi` SET `cur_epi`=`cur_epi`-1 WHERE `id` = ?", (str(bid),) )
-    sqlcon.commit()
+    db.connect()
+    bgm = Bangumi.get(Bangumi.id==bid)
+    bgm.cur_epi = bgm.cur_epi -1
+    bgm.save()
+    db.close()
 
 def chkup( bid ):
-    name = sqlcur.execute("SELECT `name`, `chk_key`, `cur_epi`, `on_air_day` FROM `bangumi` WHERE `id` = ?", (str(bid),) ).fetchone()
-    if not name[1] or not name[3]:
-        return 0
-    tepi = int(name[2])+1
-    chkkey = str(name[1]) + " " + "%02d MP4"%tepi
-    name = name[0]
+    db.connect()
+
+    bgm = Bangumi.get( Bangumi.id==bid  )
+    db.close()
+
+    tepi = bgm.cur_epi+1
+    chkkey = str(bgm.chk_key) + " " + "%02d"%tepi
+    # name = bgm.name
     chkkey = _up.quote_plus(chkkey)
+
     r = _ur.urlopen( "http://share.popgo.org/search.php?title=%s&sorts=1"%chkkey ).read().decode('utf-8')
     re1 = _re.compile( '查看详情页.*?title="([^"]*' + '%02d'%tepi + '[集话\]】\[][^"]*)".*?(magnet[^"]*)"' )
     maglink = _re.search(re1, r)
