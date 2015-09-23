3ae9f8b9c3381872a0b384201736047cbdf43651
Archibald Lu <chienius@outlook.com>
2015-09-23 20:48:25 +0800

    [Seeker] bilibili smart detect check key to be Series ID or search keyword for backward support.

--- anicolle/seeker/bilibili.py
+++ anicolle/seeker/bilibili.py
@@ -1,20 +1,24 @@
 import requests
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from json import loads
 
 def seek(chk_key, cur_epi):
     tepi = cur_epi+1
     chk_key = str(chk_key)
 
-    season_id = chk_key
-    # query_url = "http://www.bilibili.com/search?keyword=%s&orderby=&type=series&tids=&tidsC=&arctype=all&page=1" % (chk_key, )
-    # html_content = requests.get(query_url).text
-    # bs = BeautifulSoup(html_content, "html.parser")
-    # s_bgmlist = bs.find('div', class_="s_bgmlist")
-    # try:
-        # season_id = s_bgmlist.get('data-seasonid')
-    # except AttributeError:
-        # return {}
+    try:
+        int(chk_key)
+    except ValueError:
+        query_url = "http://www.bilibili.com/search?keyword=%s&orderby=&type=series&tids=&tidsC=&arctype=all&page=1" % (chk_key, )
+        html_content = requests.get(query_url).text
+        bs = BeautifulSoup(html_content, "html.parser")
+        s_bgmlist = bs.find('div', class_="s_bgmlist")
+        try:
+            season_id = s_bgmlist.get('data-seasonid')
+        except AttributeError:
+            return {}
+    else:
+        season_id = chk_key
 
     api_url = "http://app.bilibili.com/bangumi/seasoninfo/%s.ver?callback=episodeJsonCallback" % (season_id,)
     apiRes = requests.get(api_url).text
