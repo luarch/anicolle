a77fa3f29c46cd1a77d155d47617499108cab655
Archibald Lu <chienius@outlook.com>
2015-09-23 17:23:56 +0800

    Improved bilibili seeker using Series ID as check keyword.

--- anicolle/seeker/bilibili.py
+++ anicolle/seeker/bilibili.py
@@ -1,37 +1,42 @@
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from json import loads
 
 def seek(chk_key, cur_epi):
     tepi = cur_epi+1
     chk_key = str(chk_key)
 
-    query_url = "http://www.bilibili.com/search?keyword=%s&orderby=&type=series&tids=&tidsC=&arctype=all&page=1" % (chk_key, )
-    html_content = requests.get(query_url).text
-    bs = BeautifulSoup(html_content, "html.parser")
-    s_bgmlist = bs.find('div', class_="s_bgmlist")
-    try:
-        season_id = s_bgmlist.get('data-seasonid')
-    except AttributeError:
-        return {}
+    season_id = chk_key
+    # query_url = "http://www.bilibili.com/search?keyword=%s&orderby=&type=series&tids=&tidsC=&arctype=all&page=1" % (chk_key, )
+    # html_content = requests.get(query_url).text
+    # bs = BeautifulSoup(html_content, "html.parser")
+    # s_bgmlist = bs.find('div', class_="s_bgmlist")
+    # try:
+        # season_id = s_bgmlist.get('data-seasonid')
+    # except AttributeError:
+        # return {}
 
     api_url = "http://app.bilibili.com/bangumi/seasoninfo/%s.ver?callback=episodeJsonCallback" % (season_id,)
     apiRes = requests.get(api_url).text
     apiRes = apiRes[20:]
     apiRes = apiRes[:-1]
-    #print(apiRes)
     apiRes = loads(apiRes)
     epi_list = apiRes['result']['episodes']
-    epi_list.reverse()
 
     try:
-        av_id = epi_list[tepi-1]['av_id']
-        av_page= epi_list[tepi-1]['page']
+        for epi in  epi_list:
+            if epi['index'] == str(tepi):
+                av_id = epi['av_id']
+                av_page= epi['page']
+                av_name = epi['index_title']
+                break
+        else:
+            raise IndexError;
     except IndexError:
         return 0
 
-    maglink = "http://www.bilibili.com/av%s/index_%s.html" % (av_id, av_page)
-    magname = "%s - %d from Bilibili" % (chk_key, tepi)
+    maglink = "http://www.bilibili.com/video/av%s/index_%s.html" % (av_id, av_page)
+    magname = "%s - %d from Bilibili" % (av_name, tepi)
 
     return {'maglink': maglink, 'magname': magname}
 
