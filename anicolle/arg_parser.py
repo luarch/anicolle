b5e22949132dd8909f59870f5b4431a331c32662
Archibald Lu <chienius@outlook.com>
2017-08-13 19:55:27 +0800

    Kick off r2 development
    1. Remove built-in web UI (leave REST APIs)
    2. Remove CLI
    3. Clean up code

--- anicolle/arg_parser.py
+++ anicolle/arg_parser.py
@@ -1,46 +1,12 @@
 #!/usr/bin/env python
 import argparse
 
+
 def parse_args():
     aparser = argparse.ArgumentParser()
-    aparser.add_argument(
-        "-a", "--add", nargs="?", const="1"
-    )
-    aparser.add_argument(
-        "-m", "--modify", type=int,
-        metavar="ID", help="Modify a bgm."
-    )
-    aparser.add_argument(
-        "-rm", "--remove", type=int,
-        metavar="ID", help="Delete a bgm."
-    )
-    aparser.add_argument(
-        "-p", "--plus", type=int,
-        metavar="ID", help="Plus 1 to the bgm specified."
-    )
-    aparser.add_argument(
-        "-d", "--decrease", type=int,
-        metavar="ID", help="Decrease 1 to the bgm specified."
-    )
-    aparser.add_argument(
-        "-s", "--show", type=int,
-        metavar="ID", help="Display specified bangumi.", default=-1
-    )
-    aparser.add_argument(
-        "-t", "--showbyday", type=int,
-        metavar="WEEKDAY", choices=range(0,8),
-        help="Display bangumi on specified onair day.", default=-1
-    )
-    aparser.add_argument(
-        "-c", "--chkup", type=int,
-        metavar="ID",
-        help="Checkup latest updates of the specified bangumi and returns the magnet link to download",
-        const=-1, nargs='?'
-    )
     aparser.add_argument(
         "-w", "--webui", action="store_true",
         help="Start the web-based user interface"
     )
     args = aparser.parse_args()
     return args
-
--- anicolle/webui/__init__.py
+++ anicolle/webui/__init__.py
@@ -1,98 +1,58 @@
 #!/usr/bin/env python
 from .. import core as core
 import json
-from socket import gethostname
-from bottle import run, template, request, static_file, Bottle, redirect, TEMPLATE_PATH
-from beaker.middleware import SessionMiddleware
+from bottle import run, request
+from bottle import Bottle
 
 app = Bottle()
 workDir = "./anicolle/webui/"
-TEMPLATE_PATH.append(workDir+"views")
 auth_token = core.config.AUTH_TOKEN
 
-session_opts = {
-    'session.type': 'file',
-    'session.cookie_expires': False,
-    'session.data_dir': './data',
-    'session.auto': True
-}
-
-app_s = SessionMiddleware(app, session_opts)
 
 def auth(callback):
     def decorator(*args, **kwargs):
-        s = request.environ.get('beaker.session')
-        if (not 'token' in  s) or s['token']!=auth_token:
-            auth = request.get_header("X-Auth-Token")
-            if auth == auth_token:
-                return callback(*args, **kwargs)
-            else:
-                return redirect('/login')
-        else:
+        auth = request.get_header("X-Auth-Token")
+        if auth == auth_token:
             return callback(*args, **kwargs)
+        else:
+            return 401
     return decorator
 
-
-@app.route("/static/<path:path>")
-def static(path):
-    return static_file( path, root=workDir+'public' );
-
-@app.route("/")
-@auth
-def home():
-    return template("home", hostname = gethostname().upper() )
-
-@app.get('/login')
-def getLogin():
-    return template("login", hostname = gethostname().upper())
-
-@app.post('/login')
-def postLogin():
-    if(request.forms.token == auth_token) :
-        s = request.environ.get('beaker.session')
-        s['token'] = request.forms.token
-        s.save()
-        return redirect('/')
-    else:
-        print("[WARN] Failed to login with wrong token '", request.forms.token, "'")
-        return redirect('login')
-
-@app.get('/logout')
-def getLogout():
-        s = request.environ.get('beaker.session')
-        s.delete()
-        s.save()
-        return redirect('/')
 
 @app.route("/action/get/")
 @app.route("/action/get")
 @auth
 def getAllBgm():
-    return json.dumps( core.getAni() )
+    return json.dumps(core.getAni())
+
 
 @app.route("/action/get/<bid>")
 @auth
-def getBgm( bid ):
-    return json.dumps( core.getAni(int(bid)) )
+def getBgm(bid):
+    return json.dumps(core.getAni(int(bid)))
+
 
 @app.route("/action/plus/<bid>")
 @auth
-def plus( bid ):
+def plus(bid):
     core.increase(bid)
+
 
 @app.route("/action/decrease/<bid>")
 @auth
-def decrease( bid ):
+def decrease(bid):
     core.decrease(bid)
+
 
 @app.post("/action/modify/<bid>")
 @auth
-def modify( bid ):
+def modify(bid):
     name = request.forms.name
     cur_epi = request.forms.cur_epi
     on_air = request.forms.on_air
     seeker = json.loads(request.forms.seeker)
-    core.modify( bid, name, cur_epi, on_air, seeker )
+    core.modify(bid, name, cur_epi, on_air, seeker)
+
 
 @app.post("/action/add")
 @auth
@@ -101,24 +61,29 @@ def add():
     cur_epi = request.forms.cur_epi
     on_air = request.forms.on_air
     seeker = json.loads(request.forms.seeker)
-    core.create ( name, cur_epi, on_air, seeker )
+    core.create(name, cur_epi, on_air, seeker)
+
 
 @app.route("/action/remove/<bid>")
 @auth
 def remove(bid):
-    core.remove( bid );
+    core.remove(bid)
+
 
 @app.route("/action/chkup/<bid>")
 @auth
 def chkup(bid):
-    return json.dumps( core.chkup(bid) );
+    return json.dumps(core.chkup(bid))
+
 
 @app.route("/action/get_seekers/")
 @auth
 def getSeekers():
-    return json.dumps(list(core.seeker.keys()));
+    return json.dumps(list(core.seeker.keys()))
+
 
 def start(port=core.config.SERVER_PORT):
     print("Running with ", core.run_mode, " mode.")
     port = int(port)
-    run(app_s, host=core.config.SERVER_HOST, port=port, debug=core.config.DEBUG)
+    run(app, host=core.config.SERVER_HOST,
+        port=port, debug=core.config.DEBUG)
