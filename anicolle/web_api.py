df52c090a7331a7ab69a6afaaeeee834c1f1f2cc
Archibald Lu <chienius@outlook.com>
2017-08-13 20:22:36 +0800

    Add CORS for web API

--- anicolle/web_api.py
+++ anicolle/web_api.py
@@ -27,6 +27,17 @@ def auth(callback):
     return decorator
 
 
+@app.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers[
+            'Access-Control-Allow-Methods'
+            ] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers[
+            'Access-Control-Allow-Headers'
+            ] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
 @app.route("/get/")
 @app.route("/get")
 @auth
